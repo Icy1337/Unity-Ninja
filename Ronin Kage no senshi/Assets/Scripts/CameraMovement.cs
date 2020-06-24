edbd913f1667f0667afe928acb7747cb2cc17548
Arie Pinto <ariep@weizmann.ac.il>
2020-06-24 16:51:48 +0300

    Improved camera movement.

--- Ronin Kage no senshi/Assets/Scripts/CameraMovement.cs
+++ Ronin Kage no senshi/Assets/Scripts/CameraMovement.cs
@@ -6,12 +6,14 @@ public class CameraMovement : MonoBehaviour
 {
 
     public GameObject followTarget;
+    private Vector3 _cameraOffset;
     public float moveSpeed;
+    public bool rotateAroundPlayer;
+    public float rotationSpeed = 10.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        _cameraOffset = transform.position - followTarget.transform.position;    
     }
 
     // Update is called once per frame
@@ -19,8 +21,20 @@ public class CameraMovement : MonoBehaviour
     {
         if (followTarget != null)
         {
-            transform.position = Vector3.Lerp(transform.position,
-            followTarget.transform.position, Time.deltaTime * moveSpeed);
+
+            if (rotateAroundPlayer)
+            {
+                Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+                _cameraOffset = camTurnAngle * _cameraOffset;
+            }
+
+            Vector3 newPos = followTarget.transform.position + _cameraOffset;
+            transform.position = Vector3.Slerp(transform.position, newPos, Time.deltaTime * moveSpeed);
+
+            if (rotateAroundPlayer)
+            {
+                transform.LookAt(followTarget.transform);
+            }
         }
     }
 }
--- Ronin Kage no senshi/Assets/Scripts/PlayerController.cs
+++ Ronin Kage no senshi/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 50.0f;
+    public float moveSpeed = 5.0f;
     private CharacterController characterController;
+    public LayerMask layerMask;
+    private Vector3 currentLookTarget = Vector3.zero;
 
     // Use this for initialization
     void Start()
@@ -13,10 +15,29 @@ public class PlayerController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
     }
 
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+                Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        moveDirection = transform.TransformDirection(moveDirection);
         characterController.SimpleMove(moveDirection * moveSpeed);
     }
 }
